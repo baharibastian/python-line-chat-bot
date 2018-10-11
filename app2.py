beb29882f285ef3bdf32781368ab55a004a9272e
Bahari Bastian <baharibastian@Baharis-MacBook-Pro.local>
2018-10-11 14:18:15 +0700

    debug event

--- app2.py
+++ app2.py
@@ -325,6 +325,7 @@ def handle_text_message(event):
 
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
+    print("lokasi"+event.message)
     line_bot_api.reply_message(
         event.reply_token,
         LocationSendMessage(
